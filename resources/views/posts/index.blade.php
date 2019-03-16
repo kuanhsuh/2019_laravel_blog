b981ab7aa8656965628379ef209669ad4d37c295
dannyhuang <kuanhsuh@gmail.com>
2019-03-17 07:19:05 +0800

    Refactor Post From Posts Index

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -6,22 +6,7 @@
 <div class="row">
     <div class="col-lg-8 col-md-10 mx-auto">
     @foreach ($posts as $post)
-        <div class="post-preview">
-            <a href="post.html">
-                <h2 class="post-title">
-                    {{$post->title}}
-                </h2>
-                <h3 class="post-subtitle">
-                    {{$post->body}}
-                </h3>
-            </a>
-            <p class="post-meta">
-                Posted by
-                <a href="#">Start Bootstrap</a>
-                on {{$post->created_at->toFormattedDateString()}}
-            </p>
-        </div>
-        <hr />
+        @include('posts.post')
     @endforeach
         <!-- Pager -->
         <div class="clearfix">
---
+++ resources/views/posts/post.blade.php
@@ -0,0 +1,16 @@
+<div class="post-preview">
+  <a href="post.html">
+      <h2 class="post-title">
+          {{$post->title}}
+      </h2>
+      <h3 class="post-subtitle">
+          {{$post->body}}
+      </h3>
+  </a>
+  <p class="post-meta">
+      Posted by
+      <a href="#">Start Bootstrap</a>
+      on {{$post->created_at->toFormattedDateString()}}
+  </p>
+</div>
+<hr />
